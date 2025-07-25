5bff1267f266b45b497e326365f97f298aaeedca
MonzonIker <iker.monzon1999@gmail.com>
2025-07-25 02:38:51 +0200

    Primeros pasos
    creacion de entorno, pj y pickups

---
+++ scripts/player.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+var animation_speed = 2
+var moving = false
+var tile_size = 16
+var inputs = {
+	"ui_right": Vector2.RIGHT,
+	"ui_left": Vector2.LEFT,
+	"ui_up": Vector2.UP,
+	"ui_down": Vector2.DOWN
+}
+
+@onready var ray = $RayCast2d
+
+func _ready():
+	position = position.snapped(Vector2.ZERO * tile_size)
+	position += Vector2.ZERO * tile_size / 2
+	
+func _unhandled_input(event):
+	if moving:
+		return
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+			
+func move(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	$AnimatedSprite2D.play(dir)
+	if !ray.is_colliding():
+		#position += inputs[dir] * tile_size
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		await tween.finished
+		moving = false
+	$AnimatedSprite2D.play("idle "+dir)
